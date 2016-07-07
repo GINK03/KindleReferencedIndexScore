a8180882ae0781d0b8f23853311768fba3618ad5
Your Name <you@example.com>
2016-07-07 18:49:10 +0900

    add KindleWrapUp

--- KindleWrapUp.py
+++ KindleWrapUp.py
@@ -5,6 +5,7 @@ import urllib2
 import urllib
 import cPickle as pickle
 import os.path
+import __future__
 # defined parameters
 KINDLE_URL = 'https://www.amazon.co.jp/Kindle-%E3%82%AD%E3%83%B3%E3%83%89%E3%83%AB-%E9%9B%BB%E5%AD%90%E6%9B%B8%E7%B1%8D/b?ie=UTF8&node=2250738051'
 
@@ -18,6 +19,8 @@ class ScrapingData:
         self.url = 'https://'
         self.uniq_url = 'https://'
         self.date = 0
+        self.title = ''
+        self.description = ''
         self.html_raw = ''
         self.html_context = ''
         self.amazon_rating = 0
@@ -27,31 +30,34 @@ ALL_SCRAPING_DATA = []
 
 def initiate_data():
     if os.path.isfile('scraped_data.dat'):
-        with open('scraped_data.dat', 'r') as file:
-            ALL_SCRAPING_DATA = pickle.loads(file.read())
-            print ALL_SCRAPING_DATA
+        with open('scraped_data.dat', 'r') as file: 
+            for line in file: 
+                ALL_SCRAPING_DATA.append( pickle.loads(line) )
+            print( ALL_SCRAPING_DATA )
 
 def finish_procedure():
     with open('scraped_data.dat', 'w') as file:
-        file.write( pickle.dumps(ALL_SCRAPING_DATA) )
+        for scrating_data in ALL_SCRAPING_DATA:
+            file.writelines( pickle.dumps(scraping_data).replace('\n', '') + '\n' )
 
 if __name__ == '__main__':
     initiate_data()
     while(True):
         try:
+            print 'try connecting to amazon server.' 
             html = urllib2.urlopen(KINDLE_URL).read()
             soup = bs4.BeautifulSoup(html)
-        except socket.timeout, e::
-            print 'timeout!'
-            continue
+            print 'connected to amazon server.' 
         except:
-            print 'cannot connect to amazon'
+            print 'cannot connect to amazon server!' 
+            continue
         break
 
     for i, a in enumerate(soup.find_all('a')):
         if a.has_attr('href'):
+            print 'try analyising...', i, '/', len(soup.find_all('a')) 
             unquote_url = ''
-	    try:
+            try:
             	unquote_url = urllib.unquote(a['href']).encode('raw_unicode_escape').decode('utf-8')
             except:
                 unquote_url = 'None'
@@ -59,7 +65,7 @@ if __name__ == '__main__':
             description = a.string
             if 'Kindle' in unquote_url:
                 if filter(lambda x:raw_url == x[0], ALL_SCRAPING_DATA ) == []:
-                    try:
+                    #try:
                         scraping_data = ScrapingData()
                         scraping_data.url = raw_url
                         scraping_data.uniq_url = ''
@@ -67,10 +73,15 @@ if __name__ == '__main__':
                             scraping_data.uniq_url = '/'.join( raw_url.split('/')[:-1] )
                         else:
                             scraping_data.uniq_url = raw_url
-                        scraping_data.html_raw = urllib2.urlopen(raw_url).read()
+                        #scraping_data.html_raw = urllib2.urlopen(raw_url).read()
+                        # soupの保存は深度エラーになって対応できない
+                        _soup = bs4.BeautifulSoup(scraping_data.html_raw)
+                        scraping_data.title = (lambda x: _soup.title.string if x != None else 'None')(_soup.title)
                         ALL_SCRAPING_DATA.append( (scraping_data.uniq_url, scraping_data) )
-                        print 'new', scraped_data.uniq_url, description
-                    except:
+                        pickle.dumps(scraping_data) 
+                        print 'new', scraping_data.uniq_url, description 
+                    #except :
+                    #    print( 'dispose', i )
                         pass
 
     finish_procedure()
