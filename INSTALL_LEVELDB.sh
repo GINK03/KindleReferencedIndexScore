6529751a2cfbfface867e995b5721e5b439336e8
root <root@localhost.localdomain>
2016-08-04 11:06:45 +0900

    imporoved shell script

--- INSTALL_LEVELDB.sh
+++ INSTALL_LEVELDB.sh
@@ -1,6 +1,7 @@
 sudo cp ./leveldb/include/leveldb /usr/local/include -rf
 sudo cp ./leveldb/out-shared/libleveldb.* /usr/local/lib  
-sudo vi /etc/ld.so.conf.d/usrlocallib.conf
+# sudo vi /etc/ld.so.conf.d/usrlocallib.conf
 # ## add below line:
 # /usr/local/lib
 sudo ldconfig
+sudo pip install plyvel
