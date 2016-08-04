9fe584b5ad14196ea60943eb65d2c294da8798e5
root <root@localhost.localdomain>
2016-08-04 10:52:46 +0900

    add leveldb install shellscript in centos

---
+++ INSTALL_LEVELDB.sh
@@ -0,0 +1,6 @@
+sudo cp ./leveldb/include/leveldb /usr/local/include -rf
+sudo cp ./leveldb/out-shared/libleveldb.* /usr/local/lib  
+sudo vi /etc/ld.so.conf.d/usrlocallib.conf
+# ## add below line:
+# /usr/local/lib
+sudo ldconfig
