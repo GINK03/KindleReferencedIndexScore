d7ebd90688bab9ed314095ecc27f011f82a7be80
gink03 <angeldust03@gmail.com>
2016-08-09 20:29:30 +0900

    update redis install sh

--- INSTALL_REDIS.sh
+++ INSTALL_REDIS.sh
@@ -3,4 +3,5 @@ sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-*.rpm
 yum -y install redis
 systemctl start redis.service
 systemctl enable redis.service
-
+# edit /etc/redis.conf
+# bind 127.0.0.1 0.0.0.0
