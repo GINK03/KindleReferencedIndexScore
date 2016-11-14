0e078c6911dc5b187ed5f1a1e7df49ec002b3403
Your Name <you@example.com>
2016-11-15 00:50:14 +0900

    add result

--- query-expansion/chainer-char-rnn/proof_reading_evaluation.py
+++ query-expansion/chainer-char-rnn/proof_reading_evaluation.py
@@ -2,13 +2,29 @@
 import os
 import sys
 import subprocess
+import argparse
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rnn_num', type=int, default=128)
+    parser.add_argument('--target',  type=str, default='piconews')
+    args = parser.parse_args()
+    rnn_num = args.rnn_num
+    target  = args.target
+
     for i, line in enumerate(sys.stdin):
         line = line.strip()
+    
         text = line
-        res = subprocess.check_output(['python', 'proofreading.py', '--vocabulary', './data/nanoproduction/vocab.bin', '--model', 'cv/latest_512.chainermodel', '--primetext', '1', '--gpu', '-1', '--length','15', '--text', text])
-        print res
+        res = subprocess.check_output(['python', 'proofreading.py', '--vocabulary', \
+                                                                    './data/' + target + '/vocab.bin', \
+                                                                    '--model', 'cv/latest_' + str(rnn_num) + '.chainermodel', \
+                                                                    '--primetext', '1', \
+                                                                    '--gpu', '-1', \
+                                                                    '--length','15', \
+                                                                    '--text', text])
         ps_raw = filter(lambda x: 'chosen_ps' in x , res.split('\n'))
-        print i, ps_raw.pop()
+        ps_raw = ps_raw.pop().split('=').pop()
+        ps = map(float, ps_raw.strip().split(' ') )
+        print i, ",", sum(ps)/len(ps)
 
