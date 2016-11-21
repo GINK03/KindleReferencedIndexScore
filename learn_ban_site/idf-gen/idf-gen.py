534586dc3abed706230ac81d47a5ddc696c5d445
Your Name <you@example.com>
2016-11-21 18:47:40 +0900

    a

--- learn_ban_site/idf-gen/idf-gen.py
+++ learn_ban_site/idf-gen/idf-gen.py
@@ -9,13 +9,14 @@ import MeCab
 
 
 tagger = MeCab.Tagger('-Owakati')
-alltext = open('./positive.sites.20161121.json').read() + '\n' + open('./negative.sites.20161121.json').read()
+alltext = open('./positive.sites.20161121.customjson').read() + '\n' + open('./negative.sites.20161121.customjson').read()
 dic = {}
 count = 0
 for index, line in enumerate( alltext.split('\n') ):
+    raw = ' '.join(line.split(' ')[1:])
     count += 1
     try:
-      obj = json.loads(line)
+      obj = json.loads(raw)
     except ValueError, e:
       continue 
     body = obj['b']
@@ -33,7 +34,7 @@ for index, line in enumerate( alltext.split('\n') ):
 idf = {"___DOC_NUM___": count}
 term_num = {}
 for term, s in dic.iteritems():
-   idf[term] = float(count) / math.log(len(s))
+   idf[term] = math.log( float(count) / len(s) )
    term_num[term] = len(s)
 
 print open('idf.json', 'w').write(json.dumps(idf))
