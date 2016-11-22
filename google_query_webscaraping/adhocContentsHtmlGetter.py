c33716adfdccceb34d82bd2033edf51b831c803d
Your Name <you@example.com>
2016-11-22 14:28:27 +0900

    update ecraper

--- google_query_webscaraping/adhocContentsHtmlGetter.py
+++ google_query_webscaraping/adhocContentsHtmlGetter.py
@@ -17,7 +17,7 @@ def html_adhoc_fetcher(url):
         #opener = urllib2.build_opener()
         try:
             TIME_OUT = 2.5
-            print url, _
+            #print url, _
             req = urllib2.Request(url, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'} )
             html = urllib2.urlopen(req, timeout=TIME_OUT).read()
             pass
@@ -55,33 +55,62 @@ def html_adhoc_fetcher(url):
     line = regex.sub('<!--.*?-->', '',  line)
     line = regex.sub('<style.*?/style>', '',  line)
     html = regex.sub('<script.*?/script>', '', line ).replace('^A^B^C', ' ')
- 
-    soup = bs4.BeautifulSoup(html)
+    try: 
+        soup = bs4.BeautifulSoup(html)
+    except:
+        print "HTMLのパースに失敗しました"
+        return None
     title = (lambda x:unicode(x.string) if x != None else 'Untitled')( soup.title )
     anchor = ' '.join([a.text for a in soup.findAll('a') ])
     urls = []
     for a in soup.find_all('a', href=True):
         urls.append( a.get('href').encode('utf-8') )
-
+    """ アンカータグ a-tagを削除 """
+    for t in soup.findAll("a"):
+        t.extract()
+    # soup.renderContents()
     body = (lambda x:x.text if x != None else "" )(soup.find('body') )
     return title, anchor, urls, body
 #soup = bs4.BeautifulSoup(html)
 import feedparser
 import sys
 import regex
+import urllib
 # ポジティブIDF辞書作成モード
 if '-gq' in sys.argv:
     import json
-    for line in ['いずこ']:
-        line = regex.sub('\s{1,}', ' ', line)
-        reses = html_adhoc_fetcher('https://www.google.co.jp/search?q=test')
+    import plyvel 
+    db = plyvel.DB("正しい日本語.ldb", create_if_missing=True, error_if_exists=False)
+    nps = filter(lambda x:x!=('', ), [tuple(l.split('\t') ) for l in open('./negative_positive.dat').read().split('\n')] )
+    #print nps
+    for line in nps:
+        line = line[1] #""" 一番目のインデックスは、用法が含まれたデータになっているはず """
+        encoded = urllib.quote(line)
+        reses = html_adhoc_fetcher('https://www.google.co.jp/search?q=' + encoded + '&num=50')
         title  = reses[0]
         anchor = reses[1]
         urls   = reses[2]
+        urls   = filter(lambda x:'google' not in x and 'pdf' not in x, filter(lambda x: regex.match('^http', x) , urls)  )
         body   = reses[3]
-        print title
-        print urls
-
+        for enum, url in enumerate(urls):
+            if db.get(url) != None:
+                print enum, line, "すでに解析済みです"
+                continue
+            reses2 = html_adhoc_fetcher(url)
+            if reses2 == None:
+                print "htmlの取得ができませんでした"
+                db.put(url, "___error1___")
+                continue
+            title2  = reses2[0].encode('utf-8')
+            anchor2 = reses2[1]
+            urls2   = reses2[2]
+            body2   = reses2[3].encode('utf-8')
+            """ 正しい情報に書き換え """
+            for np in nps:
+                n, p = np
+                body2= regex.sub(n, p, body2)
+            if line in body2:
+              db.put(url, body2.replace(line, "<<<" + line + ">>>") )
 if '-gd' in sys.argv:
     db = plyvel.DB('posi_contents.ldb')
     import json
