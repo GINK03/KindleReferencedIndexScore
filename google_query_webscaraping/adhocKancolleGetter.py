9d0ecf8d8cc105f2912d700d22c04cc141ffea68
G K <none@none>
2016-11-28 12:18:50 +0900

    imporoved

--- google_query_webscaraping/adhocKancolleGetter.py
+++ google_query_webscaraping/adhocKancolleGetter.py
@@ -3,9 +3,9 @@ import os
 import math
 import sys
 import regex
-import urllib2
-import urllib2
-import httplib
+import urllib.request, urllib.error, urllib.parse
+import urllib.request, urllib.error, urllib.parse
+import http.client
 import ssl
 import multiprocessing as mp
 from socket import error as SocketError
@@ -16,56 +16,65 @@ def html_adhoc_fetcher(url):
     for _ in range(3):
         #opener = urllib2.build_opener()
         try:
-            TIME_OUT = 3.0
-            #print url, _
-            req = urllib2.Request(url, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'} )
-            html = urllib2.urlopen(req, timeout=TIME_OUT).read()
+            TIME_OUT = 6.0
+            print(url, _)
+            req = urllib.request.Request(url, headers = {'User-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36'} )
+            #req = urllib.request.Request(url )
+            html = urllib.request.urlopen(req, timeout=TIME_OUT).read()
             pass
-        except EOFError, e:
-            print('[WARN] Cannot access url with EOFError, try number is...', e, _, url, mp.current_process() )
+        except EOFError as e:
+            print(('[WARN] Cannot access url with EOFError, try number is...', e, _, url, mp.current_process() ))
             continue
-        except urllib2.URLError, e:
-            print('[WARN] Cannot access url with urllib2.URLError, ', e, 'reason', e.reason, 'num', _, url, mp.current_process() )
+        except urllib.error.URLError as e:
+            print(('[WARN] Cannot access url with urllib2.URLError, ', e, 'reason', e.reason, 'num', _, url, mp.current_process() ))
             continue
-        except urllib2.HTTPError, e:
-            print('[WARN] Cannot access url with urllib2.httperror, try number is...', e, _, url, mp.current_process() )
+        except urllib.error.HTTPError as e:
+            print(('[WARN] Cannot access url with urllib2.httperror, try number is...', e, _, url, mp.current_process() ))
             continue
-        except ssl.SSLError, e:
-            print('[WARN] Cannot access url with ssl error, try number is...', e, _, url, mp.current_process() )
+        except ssl.SSLError as e:
+            print(('[WARN] Cannot access url with ssl error, try number is...', e, _, url, mp.current_process() ))
             continue
-        except ssl.CertificateError, e:
-            print('[WARN] Cannot access url with ssl error, try number is...', e, _, url, mp.current_process() )
+        except ssl.CertificateError as e:
+            print(('[WARN] Cannot access url with ssl error, try number is...', e, _, url, mp.current_process() ))
             continue 
-        except httplib.BadStatusLine, e:
-            print('[WARN] Cannot access url with BadStatusLine, try number is...', e, _, url, mp.current_process() )
+        except http.client.BadStatusLine as e:
+            print(('[WARN] Cannot access url with BadStatusLine, try number is...', e, _, url, mp.current_process() ))
             continue
-        except httplib.IncompleteRead, e:
-            print('[WARN] Cannot access url with IncompleteRead, try number is...', e, _, url, mp.current_process() )
+        except http.client.IncompleteRead as e:
+            print(('[WARN] Cannot access url with IncompleteRead, try number is...', e, _, url, mp.current_process() ))
             continue
-        except SocketError, e:
-            print('[WARN] Cannot access url with SocketError, try number is...', e, _, url, mp.current_process() )
+        except SocketError as e:
+            print(('[WARN] Cannot access url with SocketError, try number is...', e, _, url, mp.current_process() ))
             continue
-        except UnicodeEncodeError, e:
-            print('[WARN] Cannot access url with UnicodeEncodeError, try number is...', e, _, url, mp.current_process() )
+        except UnicodeEncodeError as e:
+            print(('[WARN] Cannot access url with UnicodeEncodeError, try number is...', e, _, url, mp.current_process() ))
             continue
         break
     if html == None:
         return None
-    line = html.replace('\n', '^A^B^C')
-    line = regex.sub('<!--.*?-->', '',  line)
-    line = regex.sub('<style.*?/style>', '',  line)
-    html = regex.sub('<script.*?/script>', '', line ).replace('^A^B^C', ' ')
+    #line = regex.sub('\n', '^A^B^C', html)
+    #line = regex.sub('<!--.*?-->', '',  line)
+    #line = regex.sub('<style.*?/style>', '',  line)
+    #html = regex.sub('<script.*?/script>', '', line ).replace('^A^B^C', ' ')
+    #print(html)
+    #sys.exit(0)
+    soup = bs4.BeautifulSoup(html)
+    print(soup.title)
     try: 
         soup = bs4.BeautifulSoup(html)
     except:
-        print "HTMLのパースに失敗しました"
+        print("HTMLのパースに失敗しました")
         return None
-    title = (lambda x:unicode(x.string) if x != None else 'Untitled')( soup.title )
+    title = (lambda x:str(x.string) if x != None else 'Untitled')( soup.title )
     anchor = 'ダミー'
     """anchor = ' '.join([a.text for a in soup.findAll('a') ])"""
     urls = []
+    print(str(html))
     for a in soup.find_all('a', href=True):
-        urls.append( a.get('href').encode('utf-8') )
+        urls.append( a.get('href') )
+        print("debug", a.get("href"))
+
+    sys.exit()
     """ アンカータグ a-tagを削除 """
     for t in soup.findAll("a"):
         t.extract()
@@ -73,55 +82,58 @@ def html_adhoc_fetcher(url):
     body = (lambda x:x.text if x != None else "" )(soup.find('body') )
     return title, anchor, urls, body
 #soup = bs4.BeautifulSoup(html)
-import feedparser
 import sys
 import regex
-import urllib
+import urllib.request, urllib.parse, urllib.error
 # ポジティブIDF辞書作成モード
 if '-gq' in sys.argv:
     import json
     import plyvel 
     db = plyvel.DB("kancolle.ldb", create_if_missing=True, error_if_exists=False)
     #nps = filter(lambda x:x!=('', ), [tuple(l.split('\t') ) for l in open('./negative_positive.dat').read().split('\n')] )
-    nps = filter(lambda x:x!=('', ), [tuple(l.split('\t') ) for l in open('./kancolle.dat').read().split('\n')] )
+    print(open('./kancolle.dat').read())
+    nps = open('./kancolle.dat').read().split('\n')
+    print(nps)
     #print nps
     for line in nps:
-        query = line[0]
-        print query
-        encoded = urllib.quote('艦これ+かわいい+' + query)
-        reses = html_adhoc_fetcher('https://www.google.co.jp/search?q=' + encoded + '&num=100')
+        query = line.strip()
+        print(query)
+        encoded = '+'.join(map(urllib.parse.quote_plus, ['艦これ', 'かわいい', query]))
+        #encoded = "kaii"
+        reses = html_adhoc_fetcher('https://www.google.co.jp/search?client=ubuntu&channel=fs&q=' + encoded + '&num=10')
         if reses == None:
             continue
         title  = reses[0]
         anchor = reses[1]
         urls   = reses[2]
-        urls   = filter(lambda x:'google' not in x and 'pdf' not in x and 'youtube' not in x, filter(lambda x: regex.match('^http', x) , urls)  )
         body   = reses[3]
+        print(urls)
         for enum, url in enumerate(urls):
+            url = bytes(url, 'utf-8')
             if db.get(url) != None:
-                print enum, line, "すでに解析済みです"
+                print(enum, line, "すでに解析済みです")
                 continue
             reses2 = html_adhoc_fetcher(url)
             if reses2 == None:
-                print "htmlの取得ができませんでした"
+                print("htmlの取得ができませんでした")
                 db.put(url, "___error1___")
                 continue
-            title2  = reses2[0].encode('utf-8')
+            title2  = reses2[0]
             anchor2 = reses2[1]
             urls2   = reses2[2]
-            body2   = reses2[3].encode('utf-8')
+            body2   = reses2[3]
             if query not in body2:
                 continue
             rures   = [("\s{1,}", " "), ("\n", " "), ("\d\d\d\d.*?:\d\d:\d\d", " "), ("<.*?>", " ")]
             for s, t in rures:
                 body2 = regex.sub(s, t, body2)
             body2 = body2.lower()
-            print enum, query, "scripting 完了です"
+            print(enum, query, "scripting 完了です")
             db.put(url, body2 )
 if '-gd' in sys.argv:
     import plyvel
     import json
     db = plyvel.DB('kancolle.ldb')
     for ind, (k, v) in enumerate(db):
-        print k, ind, v
+        print(k, ind, v)
 
