a743649dfc18ff9b999a1e9bb0e603ae16ae5820
g k <you@example.com>
2016-11-29 16:49:43 +0900

    add dena scraper

--- dena/adhocDeNAParser.py
+++ dena/adhocDeNAParser.py
@@ -14,12 +14,11 @@ import bs4
 def html_adhoc_fetcher(url):
     html = None
     for _ in range(5):
-        opener = urllib2.build_opener()
-        TIME_OUT = 5
-        opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36')]
-        html = opener.open(url, timeout = TIME_OUT).read()
-        print 'raw html', html, 'try', _
         try:
+            opener = urllib2.build_opener()
+            TIME_OUT = 5
+            opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36')]
+            html = opener.open(url, timeout = TIME_OUT).read()
             pass
         except EOFError, e:
             print('[WARN] Cannot access url with EOFError, try number is...', e, _, url, mp.current_process() )
@@ -48,22 +47,18 @@ def html_adhoc_fetcher(url):
         break
     if html == None:
         return None
-    print html, len(html)
     line = html.replace('\n', '^A^B^C')
     line = regex.sub('<!--.*?-->', '',  line)
     line = regex.sub('<style.*?/style>', '',  line)
     html = regex.sub('<script.*?/script>', '', line ).replace('^A^B^C', ' ')
  
-    print html
     soup = bs4.BeautifulSoup(html)
     title = (lambda x:unicode(x.string) if x != None else 'Untitled')( soup.title )
-    #print soup.text.encode('utf-8')
-    #print html
     contents0_text = (lambda x:x.text.encode('utf-8') if x != None else "dummy" )( soup.find('div', {'class': 'ui-main-column'}) )
-    print contents0_text
+    #print contents0_text
     if contents0_text == 'dummy':
         contents0_text = (lambda x:x.text.encode('utf-8') if x != None else "dummy" )( soup.find('article') )
-        print contents0_text
+        #print contents0_text
     links = list(set([a['href'] for a in soup.find_all('a', href=True)]) )
     #print links
     return title, contents0_text, links
@@ -80,7 +75,7 @@ def isValidURL(link, prefix):
       #print "invalid url", link
       return False
   # この文章が入っていたら、imageなので、無視する
-  if 'image' in link or '.jpg' in link:
+  if 'image' in link or '.jpg' in link or 'hatena'  in link or 'facebook' in link or 'twitter' in link or 'favorite' in link:
       return False
   if db.get(str(link)) != None: 
       #print "invalid url(db)", link
@@ -113,7 +108,8 @@ if '-c' in sys.argv:
           if isValidURL(link, prefix) == False: 
               continue 
           tp = html_adhoc_fetcher(link)
-
+          import time 
+          time.sleep(0.100)
           if tp == None:
             continue
           title, contents0, ls = tp
@@ -124,16 +120,15 @@ if '-c' in sys.argv:
             if isValidURL(l, prefix):
                 links.add(l)
             else:
-                print "error", l
-          print link, title, "パースしたよ, 残リンク数", len(links), ls, contents0
+                pass
+          print link, title, "パースしたよ, 残リンク数", len(links)
+          print contents0
           contents = contents0
           db.put(str(link), contents )
           import json
           db.put('___META_URLS___', json.dumps(list(links)) )
 # ダンプモード
 if '-d' in sys.argv:
-  for url, contents in db:
-    if 'iemo.jp' in url and '.jpg' not in url:
-      for line in filter(lambda x: "。" in x, contents.split(' ') ):
-          print line
-
+  for index, (url, contents) in enumerate(db):
+    if 'dummy' not in contents:
+        print index, regex.sub('\n', ' ', regex.sub('\s{1,}', ' ', contents) )
