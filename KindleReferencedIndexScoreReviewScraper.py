bcd0968ae880bbb97ca2bf64635bf3da5c07f5c8
g k <you@example.com>
2016-12-05 19:02:04 +0900

    remove extra session data

--- KindleReferencedIndexScoreReviewScraper.py
+++ KindleReferencedIndexScoreReviewScraper.py
@@ -50,7 +50,7 @@ def html_adhoc_fetcher(url):
     for _ in retrys :
 	import cookielib
 	jar = cookielib.CookieJar()
-	jar.set_cookie(makeCookie("session-token", "mu26PbfgQT4xERN6gtu8SkSoPQ7kwdgVpR7LHWqDJep5QE34DERo6AkSa0hCEhblH9k/YZkyPjnI1OIAe1+dWkZAcWZSl7fdH6SilVpQlTiFWjlsrvrvxLvb3tx2oLReL+vmG1KVWqQngeJ0JzL1asxIc+ktBPwbu7J8DPkOq05vckf90UUTPYCi6EFG2KmCYj7Yy86T9lc/CiGp7ZOUswI4cLM6rt4mKUEqnhDmpSvWKqaPWsT9EWSHXtUyyugY"))
+	jar.set_cookie(makeCookie("session-token", CM.SESSION_TOKEN))
 	headers = {"Accept-Language": "en-US,en;q=0.5","User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Referer": "http://thewebsite.com","Connection": "keep-alive" } 
 	request = urllib2.Request(url=url, headers=headers)
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(jar))
