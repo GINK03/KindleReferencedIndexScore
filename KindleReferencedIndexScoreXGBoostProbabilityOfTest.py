1647f491879b45c4da75deebad8fd6e557780d9a
Your Name <you@example.com>
2016-12-12 15:25:30 +0900

    update XGBoostProbTester

--- KindleReferencedIndexScoreXGBoostProbabilityOfTest.py
+++ KindleReferencedIndexScoreXGBoostProbabilityOfTest.py
@@ -51,6 +51,7 @@ if '--makexgboost' in sys.argv:
         it[i] = t
         ti[t] = i
     import cPickle as P
+    open('./tmp/differ.idf.p', 'w').write(P.dumps(idf))
     open('./tmp/differ.it.p', 'w').write(P.dumps(it))
     open('./tmp/differ.ti.p', 'w').write(P.dumps(ti))
     buff = []
@@ -72,30 +73,34 @@ if '--makexgboost' in sys.argv:
     random.shuffle(buff)
     open('./tmp/differ.train.svmfmt', 'w').write('\n'.join(buff[:len(buff)*3/4]))
     open('./tmp/differ.test.svmfmt', 'w').write('\n'.join(buff[len(buff)*3/4+1:]))
+    res = subprocess.check_output(['xgboost', 'xgboost.conf'])
+    print res
+    res = subprocess.check_output(['mv', './0005.model', './tmp/differ.train.svmfmt', './tmp/differ.test.svmfmt', './tmp/differ.idf.p', './tmp/differ.it.p', './tmp/differ.ti.p', '/tmp'])
+    res = subprocess.check_output(['cp', './tmp/first.txt', '/tmp'])
+    print res
+
 
 
 if '--xgboost' in sys.argv:
-    itfile = json.loads(open('./tmp/differs20161202.it.json').read())
-    tifile = json.loads(open('./tmp/differs20161202.ti.json').read())
-    idf    = json.loads(open('./tmp/differs20161202.idf.json').read())
-    raws = filter(lambda x:x != '', open('../differ/before_fix.txt').read().split('\n'))
+    import cPickle as P
+    it = P.loads(open('/tmp/differ.it.p').read())
+    ti = P.loads(open('/tmp/differ.ti.p').read())
+    idf    = P.loads(open('/tmp/differ.idf.p').read())
+    raws = filter(lambda x:x != '', open('./tmp/first.txt').read().split('\n'))
     wakatis = []
     rebuild = []
     for raw in raws:
         raw = raw.strip()
-        raw = raw.lower()
         m = MeCab.Tagger ("-Owakati")
         indexs = []
-        for k in tifile.keys():
-            pass
         wakati = []
         for t in m.parse(raw).strip().split(' '):
             wakati.append(t)
-            if tifile.get(t.decode('utf-8')) == None:
+            if ti.get(t) == None:
                 print 'error', t
                 pass
             else:
-                indexs.append( str(tifile[t.decode('utf-8')]) )
+                indexs.append( str(ti[t]) )
         wakatis.append(wakati)
         rebuild.append(' '.join(indexs))
 
@@ -108,7 +113,7 @@ if '--xgboost' in sys.argv:
         
         res = []
         for index, freq in c.items():
-            term = itfile.get(index.decode('utf-8'))
+            term = it.get(int(index))
             score = idf.get(term) * float(freq)
             res.append( [index, score] )
 
@@ -119,26 +124,10 @@ if '--xgboost' in sys.argv:
         svmfmt.append( ' '.join(map(str, dumps)) )
 
     open('/tmp/xgboost.before.check.svmfmt', 'w').write('\n'.join(svmfmt))
-
-    res = subprocess.check_output(['xgboost', '/etc/t.conf', 'task=pred', 'model_in=/tmp/0100.model'])
+    res = subprocess.check_output(['xgboost', './xgboost.conf', 'task=pred', 'model_in=/tmp/0005.model'])
 
     preds = filter(lambda x:x!= '', open('./pred.txt').read().split('\n'))
 
-    tw = {}
-    for i, w in enumerate(filter(lambda x:x!='', open('/tmp/train.txt.model').read().split('\n'))[6:]):
-        i = i + 1
-        t = itfile.get(str(i).decode('utf-8'))
-        t = t.encode('utf-8')
-        tw[t] = float(w)
-
     for p,r,w in zip(preds, raws, wakatis):
         p = float(p)
-        tp = []
-        for aw in w:
-            if tw.get(aw) != None:
-                tp.append( (aw, str(tw[aw])) )
-        tp.pop()
-        tgts = sorted(tp, key=lambda x:float(x[1]))[0:3]
-        tmp = ' '.join([x[0] + ':' + x[1] for x in tgts])
-        
-        print ''.join(map(str, [100 - int(p * 100), '%の確率で間違いです。 ', r, ' [', tmp, '] ']))
+        print ''.join(map(str, [100 - int(p * 100), '%の確率で間違いです。 ', r]))
