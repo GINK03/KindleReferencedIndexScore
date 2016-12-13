65a69b4c4701eb195a04fc930197a2ec4af82e14
Your Name <you@example.com>
2016-12-13 11:59:48 +0900

    ad logit inv

--- liblbfgs/liblinear-formatter.py
+++ liblbfgs/liblinear-formatter.py
@@ -36,17 +36,39 @@ if '-idfgen' in sys.argv:
         if 'AD_PERFORMANCE_REPORT' in line or \
                 'Ad,Headline' in line or \
                 'Ad name' in line or \
-                '.jpg' in line or \
-                '--' in line:
+                '.jpg' in line:
                     continue
-        c += 1
+        import re
+        if re.findall('".*?"', line) != []:
+            src = re.findall('".*?"', line)[-1]
+            tgt = src.replace(',', '').replace('"','')
+            rep = line.replace(src, tgt)
+            line = rep
         tp = line.split(',')
+        try:
+            imps = float(tp.pop()) # 1 impresson
+            clks = float(tp.pop()) # 2
+            cnvs = float(tp.pop()) # 3
+            acpc = float(tp.pop()) # 4
+            _    = float(tp.pop()) # 5
+            cpcv = float(tp.pop()) # 6
+            apsn = '___APOS___' + str(float(tp.pop())) 
+            excs = '___EXCUST___' + str(int(tp.pop()))
+            disp = '___URL___' + tp.pop() 
+        except:
+            print(line, line.split(','))
+            continue
+        if imps <= 5. or clks <= 5 or clks/imps >= 1.0 or clks/imps == 0.: continue
 
-        imps = float(tp.pop())
-        clks = float(tp.pop())
-
+        c += 1
         sentence = ' '.join(tp).replace(',', '')
         ts = m.parse(sentence).strip().split(' ')
+        """
+        DISP-URLを文字として認識させる
+        """
+        ts.append(disp)
+        ts.append(excs)
+        ts.append(apsn)
         for t in set(ts):
             if idf.get(t) == None:
                 idf[t] = 1
@@ -79,13 +101,19 @@ if '--shimakaze' in sys.argv:
         if 'AD_PERFORMANCE_REPORT' in line or \
                 'Ad,Headline' in line or \
                 'Ad name' in line or \
-                '.jpg' in line or \
-                '--' in line:
+                '.jpg' in line:
                     continue
         tp = line.split(',')
 
         imps = float(tp.pop())
         clks = float(tp.pop())
+        cnvs = float(tp.pop())
+        acpc = float(tp.pop())
+        _    = float(tp.pop())
+        cpcv = float(tp.pop())
+        apsn = float(tp.pop())
+        excs = int(tp.pop())
+        disp = tp.pop()
         imps_all += imps
         clks_all += clks
 
@@ -103,22 +131,48 @@ if '--tokitsukaze' in sys.argv:
         if 'AD_PERFORMANCE_REPORT' in line or \
                 'Ad,Headline' in line or \
                 'Ad name' in line or \
-                '.jpg' in line or \
-                '--' in line:
+                '.jpg' in line:
                     continue
+        import re
+        if re.findall('".*?"', line) != []:
+            src = re.findall('".*?"', line)[-1]
+            tgt = src.replace(',', '').replace('"','')
+            rep = line.replace(src, tgt)
+            line = rep
         tp = line.split(',')
+        try:
+          imps = float(tp.pop()) # 1 impresson
+          clks = float(tp.pop()) # 2
+          cnvs = float(tp.pop()) # 3
+          acpc = float(tp.pop()) # 4
+          _    = float(tp.pop()) # 5
+          cpcv = float(tp.pop()) # 6
+          apsn = '___APOS___' + str(float(tp.pop())) 
+          excs = '___EXCUST___' + str(int(tp.pop()))
+          disp = '___URL___' + tp.pop() 
+        except:
+          #print( line )
+          continue
 
-        imps = float(tp.pop())
-        clks = float(tp.pop())
+        if imps <= 5. or clks <= 5 or clks/imps >= 1.0 or clks/imps == 0.: continue
 
         sentence = ' '.join(tp).replace(',', '')
         ts = m.parse(sentence).strip().split(' ')
+        """
+        DISP-URLを文字として認識させる
+        """
+        ts.append(disp)
+        ts.append(excs)
+        ts.append(apsn)
         tf = C(ts)
         output = []
         for t,f in tf.items():
-          output.append( ':'.join(map(str, [ti[t], idf[t]*f])) )
+          """
+          1に正則化かしないか、決める
+          """
+          #output.append( ':'.join(map(str, [ti[t], idf[t]*f])) )
+          output.append( ':'.join(map(str, [ti[t], 1.])) )
         output_txt = " ".join(sorted(output, key=lambda x:int(x.split(':')[0])))
-        if imps <= 5. or clks <= 5 or clks/imps >= 1.0 or clks/imps == 0.: continue
         print(clks/imps, output_txt)
 
 if '--yukikaze' in sys.argv:
@@ -136,6 +190,17 @@ if '--yukikaze' in sys.argv:
     for t, w in sorted(tw.items(), key=lambda x:x[1]):
         print(t,w)
 
-            
-
+if '--kagerou' in sys.argv:
+    import cPickle as P
+    it = P.loads(open('it.p').read())
+    tw = {}
+    lines = open('result.txt').read().split('\n')
+    weights = map(float, lines[1].split(','))
+    for i, w in enumerate(weights):
+        tw[it[i]] = w
+    for t, w in sorted(tw.items(), key=lambda x:x[1]*-1):
+        print(t,w)
+    print(len(weights))
+    print(len(it.keys()))
+    #print((set(it.values()) - set(tw.keys())).pop())
         
