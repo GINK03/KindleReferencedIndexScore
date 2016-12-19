93ee3c9a9d4b2f66abe280c07caa0b0fbb717a53
Your Name <you@example.com>
2016-12-19 16:00:25 +0900

    update ConditionalRandomField

--- query-expansion/chainer-term-rnn/ConditionalRandomField.py
+++ query-expansion/chainer-term-rnn/ConditionalRandomField.py
@@ -11,120 +11,65 @@ from chainer import cuda, Variable, FunctionSet
 import chainer.functions as F
 from CharRNN import CharRNN, make_initial_state
 
-#sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
-
-#%% arguments
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--model',      type=str,   required=True)
-parser.add_argument('--vocabulary', type=str,   required=True)
-
-parser.add_argument('--seed',       type=int,   default=123)
-parser.add_argument('--sample',     type=int,   default=1)
-parser.add_argument('--primetext',  type=str,   default='')
-parser.add_argument('--length',     type=int,   default=2000)
-parser.add_argument('--gpu',        type=int,   default=-1)
-
-args = parser.parse_args()
-
-np.random.seed(args.seed)
-
-# load vocabulary
-vocab = pickle.load(open(args.vocabulary, 'rb'))
-#print(vocab)
-#print(' '.join(vocab.keys()))
+vocab = pickle.load(open('/home/gimpei/KindleReferencedIndexScore/query-expansion/chainer-term-rnn/data/middlesuumo/vocab.bin', 'rb'))
 ivocab = {}
 for e, (c, i) in enumerate(vocab.items()):
-    #print e, c.decode('utf-8')
     ivocab[i] = c
-## sys.exit(0)
-# load model
-model = pickle.load(open(args.model, 'rb'))
+model = pickle.load(open('/home/gimpei/KindleReferencedIndexScore/query-expansion/chainer-term-rnn/data/middlesuumo/latest_middlesuumo_768.chainermodel', 'rb'))
 n_units = model.embed.W.data.shape[1]
-
-if args.gpu >= 0:
-    cuda.get_device(args.gpu).use()
-    model.to_gpu()
-
-# initialize generator
-state = make_initial_state(n_units, batchsize=1, train=False)
-if args.gpu >= 0:
-    for key, value in list(state.items()):
-        value.data = cuda.to_gpu(value.data)
-
 prev_char = np.array([0], dtype=np.int32)
-if args.gpu >= 0:
-    prev_char = cuda.to_gpu(prev_char)
-
-if len(args.primetext) > 0:
-    #for i in unicode(args.primetext, 'utf-8'):
-    for i in ['2']:
-        #sys.stdout.write(i)
-        print(i.decode('utf-8'))
-        #prev_char = np.ones((1,), dtype=np.int32) * vocab[i.encode('utf-8')]
-        prev_char = np.ones((1,), dtype=np.int32) * vocab[i]
-        print('prev char ', prev_char)
-        if args.gpu >= 0:
-            prev_char = cuda.to_gpu(prev_char)
-
-        state, prob = model.forward_one_step(prev_char, prev_char, state, train=False)
 """
 校正対象の文字を読み込み
 """
-import MeCab
+class TextList:
+    data = ['これ', 'は', 'デフォルト', 'の', '情報', 'です']
+    before_rank = None
+    before_prob = None
+    state = None
+    @staticmethod
+    def update_data(inputs=""):
+        from MeCab import Tagger
+        t = Tagger('-Owakati')
+        data = [term if vocab.get(term) != None else "___UNK___" for term in t.parse(inputs).strip().split(' ')]
+        TextList.data = data
+        #print(data)
+    @staticmethod
+    def init_state():
+        TextList.state =  make_initial_state(n_units, batchsize=1, train=False)
+    @staticmethod
+    def process_state():
+        for index,term in [(vocab.get(term),term) for term in TextList.data]:
+            if index == None:
+                print(term, '0', 'undefined')
+                continue
+
+            char = np.array([index], dtype=np.int32)
+            TextList.state, prob = model.forward_one_step(char, char, TextList.state, train=False)
+            probability = cuda.to_cpu(prob.data)[0].astype(np.float64)
+            probability /= np.sum(probability)
+            prob_with_term = []
+            for e, p in enumerate(probability):
+                prob_with_term.append( [p, ivocab[e] ] )
+            prob_with_term = sorted(prob_with_term, key=lambda x:-1 * x[0] )
+            if TextList.before_rank == None:
+                print(ivocab[index], "this is head")
+            else:
+                print(''.join(map(str,[ivocab[index],":",TextList.before_prob[index]])), ' '.join([':'.join(map(str, [(lambda x:x if x not in ['', ' ', '　'] else '<space>')(tp[1]), str(tp[0])[:10] ] )) for tp in TextList.before_rank[:10]]) )
+            TextList.before_prob = probability
+            TextList.before_rank = prob_with_term
+        print()
+        return TextList.state
+
 if __name__ == '__main__':
-   m = MeCab.Tagger('-Owakati')
-   print(m.parse('私の名前は'))
+    for line in sys.stdin:
+        TextList.init_state()
+        line = line.strip()
+        TextList.update_data(line)
+        print(line)
+        state = TextList.process_state()
 
-prev_char_stack = []
-result = []
-for j in range(1):
-  state = make_initial_state(n_units, batchsize=1, train=False)
-  #print('\niter ', j) 
-  print('\n dumping...', i, end= " ")
-  for i in range(300):
-    if i == 0 and j != 0:
-      import random
-      for _pc in prev_char_stack[-int(50*random.random()):]:
-        state, prob = model.forward_one_step(_pc, _pc, state, train=False)
-    else:
-      state, prob = model.forward_one_step(prev_char, prev_char, state, train=False)
-    #if args.sample > 0:
-    probability = cuda.to_cpu(prob.data)[0].astype(np.float64)
-    probability /= np.sum(probability)
-    prob_with_index = []
-    for e, p in enumerate(probability):
-        prob_with_index.append( [e, p, ivocab[e] ] )
-    prob_with_index.sort(key=lambda x:-1 * x[1] )
-    """
-    ランダムチョイス
-    """
-    index = np.random.choice(list(range(len(probability))), p=probability)
-    """
-    最大値チョイス(結構うまくいく)
-    """
-    index = prob_with_index[0][0]
-    chosen_p = probability[index]
-        
-    #print("try", i, "vocindex", index, "probability", chosen_p, "voc", ivocab[index])
-    print(ivocab[index], end="")
-    #print(ivocab[index], ivocab[index] in ['こ', 'そ', '俺', '魔'] )
-    result.append(ivocab[index])
-    top_n_p_sum = 0.
-    n_p_var = np.var(probability)
-    for e, p, t in prob_with_index[0:10]:
-        #print("candidate", e, p, t)
-        top_n_p_sum += p
-    top_n_p_sum /= 10.
-    #print("  average of top 10's prob sum", top_n_p_sum)
-    #print("  probability's variance ", n_p_var)
-    prev_char = np.array([index], dtype=np.int32)
-    if args.gpu >= 0:
-        prev_char = cuda.to_gpu(prev_char)
-    
-    prev_char_stack.append(prev_char)
 
-print()
-import regex
-print( regex.sub('\s','\n ', ''.join(result)) )
-print( ''.join(result) )
+if __name__ == '__test__':
+    TextList.init_state()
+    TextList.update_data("この情報は初期値なので任意の文字列に入れ替えてください。fuckin hot")
+    state = TextList.process_state()
