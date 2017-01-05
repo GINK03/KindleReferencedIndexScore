1f22ce4fbaa0f22bde20db2710eae090c94796b4
G K <none@none>
2017-01-05 11:42:29 +0900

    improved

--- query-expansion/chainer-term-rnn/splitter.py
+++ query-expansion/chainer-term-rnn/splitter.py
@@ -9,22 +9,30 @@ from itertools import chain
 buff = []
 stringbuff = ''
 cnt = 1
-TARGET_DIR = './data/middlesuumo/'
-FILE_NAME  = 'input.txt'
+TARGET_DIR = './data/tinyasahi/'
+FILE_NAME  = 'asahi.txt.test.wakati'
+def flash(buff):
+    global cnt
+    flatten = list(chain(*buff))
+    obj = {}
+    bow = {}
+    for k, v in C(flatten).items():
+        bow[k] = v
+    obj['bow'] = bow
+    obj['contents'] = flatten
+    open(TARGET_DIR + str(cnt) + '.json', 'w').write(json.dumps(obj))
+    #for t in flatten:
+    #    print(t)
+    buff = []
+    stringbuff = ''
+    cnt += 1
+    return buff
+
 for line in filter(lambda x:x != '', open(TARGET_DIR + FILE_NAME).read().split('\n')):
     buff.append(line.split(' '))
     stringbuff += line
     if 1024*1024*10 < sys.getsizeof(stringbuff):
-        flatten = list(chain(*buff))
-        obj = {}
-        bow = {}
-        for k, v in C(flatten).items():
-            bow[k] = v
-        obj['bow'] = bow
-        obj['contents'] = flatten
-        open(TARGET_DIR + str(cnt) + '.json', 'w').write(json.dumps(obj))
-        #for t in flatten:
-        #    print(t)
-        buff = []
+        buff = flash(buff)
         stringbuff = ''
-        cnt += 1
+
+flash(buff)
