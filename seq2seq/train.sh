7836c8b7b4b88c29b9aafb57f08bbef250fa824d
Your Name <you@example.com>
2017-01-31 17:03:41 +0900

    add tensorflow seq2seq

---
+++ seq2seq/train.sh
@@ -0,0 +1,8 @@
+p2 translate.py --data_dir ./make-meidai-dialogue/ \
+ --train_dir ./train  \
+ --en_vocab_size 10000 \
+ --fr_vocab_size 10000 \
+ --gpu 0
+# --size 400  \
+# --num_layers 1 \
+# --batch_size 5  \
