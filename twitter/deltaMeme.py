0a14d3a1c8b28268a41a2e0d4df28fc0d3075d6e
Your Name <you@example.com>
2017-03-29 08:28:55 +0000

    update

--- twitter/deltaMeme.py
+++ twitter/deltaMeme.py
@@ -11,6 +11,9 @@ from collections import Counter as C
 import plyvel
 import pickle
 import numpy as np
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key 
+import re
 db = plyvel.DB('shadow.ldb', create_if_missing=True)
 m = MeCab.Tagger('-Owakati')
 cha = MeCab.Tagger('-Ochasen')
@@ -33,11 +36,32 @@ def shadow():
       print(gi, day, name)
       print(favs, oneshot)
 
-def test():
-  for url, oneshot in db:
-     url     = url.decode('utf-8')
-     oneshot = pickle.loads(oneshot)
-     print(url, oneshot)
+def xgboost_regression():
+  ## boto api
+  ACCESS_TOKEN = "AKIAJHOYKCP2J4UEZXRA"
+  SECRET_TOKEN = input()
+  conn = S3Connection(ACCESS_TOKEN, SECRET_TOKEN )
+  bucket = conn.get_bucket("irep-ml-twitter-mini")
+  print( bucket.list() )
+  for ki, key in enumerate(bucket.list()):
+    if ki%200 == 0:
+      print("iter %d"%ki)
+    obj = json.loads(key.get_contents_as_string().decode('utf-8'))
+    #print( re.search(r'(^...._..._..)_', key.key).group(1) )
+    print(obj['favs'], m.parse(obj['txt']).strip())
+
+def w2v_s3():
+  ## boto api
+  ACCESS_TOKEN = "AKIAJHOYKCP2J4UEZXRA"
+  SECRET_TOKEN = input()
+  conn = S3Connection(ACCESS_TOKEN, SECRET_TOKEN )
+  bucket = conn.get_bucket("irep-ml-twitter-mini")
+  #key_   = Key(bucket)
+  for key in bucket.list():
+     obj = json.loads(key.get_contents_as_string().decode('utf-8'))
+     print( re.search(r'(^...._..._..)_', key.key).group(1) )
+     
+     print(obj['favs'], obj['txt'])
 
 # この時のiterは停止する
 def w2v():
@@ -134,5 +158,9 @@ if __name__ == '__main__':
     test()
   if '--w2v' in sys.argv:
     w2v()
+  if '--w2v_s3' in sys.argv:
+    w2v_s3()
+  if '--xgboost_regression' in sys.argv:
+    xgboost_regression()
   if '--rel' in sys.argv:
     relevancy()
