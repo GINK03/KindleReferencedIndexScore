bc75f0464bb998828092c20de017b1769844089a
gimpei <gim.kobayashi@gmail.com>
2017-05-10 20:35:23 +0900

    change to py3

--- suumo_journal/adhocYomiuri.py
+++ suumo_journal/adhocYomiuri.py
@@ -1,126 +1,96 @@
 # coding: utf-8
+
 import os
 import math
 import sys
-import regex
-import urllib2
-import urllib2
-import httplib
+import re
+import urllib.request, urllib.error, urllib.parse
+import urllib.request, urllib.error, urllib.parse
+import http.client
 import ssl
 import multiprocessing as mp
 from socket import error as SocketError
 import bs4
+from threading import Thread as T
+from threading import active_count as CT
+import time
 
-def html_adhoc_fetcher(url):
-    html = None
-    for _ in range(5):
-        proxy_support = urllib2.ProxyHandler({'http': 'http://153.149.157.41:8086'})
-        opener = urllib2.build_opener(proxy_support)
-        TIME_OUT = 10.
-        try:
-            html = opener.open(str(url), timeout = TIME_OUT).read()
-        except EOFError, e:
-            print('[WARN] Cannot access url with EOFError, try number is...', e, _, url, mp.current_process() )
-            continue
-        except urllib2.URLError, e:
-            continue
-        except urllib2.HTTPError, e:
-            print('[WARN] Cannot access url with urllib2.httperror, try number is...', e, _, url, mp.current_process() )
-            continue
-        except ssl.SSLError, e:
-            print('[WARN] Cannot access url with ssl error, try number is...', e, _, url, mp.current_process() )
-            continue
-        except httplib.BadStatusLine, e:
-            print('[WARN] Cannot access url with BadStatusLine, try number is...', e, _, url, mp.current_process() )
-            continue
-        except httplib.IncompleteRead, e:
-            print('[WARN] Cannot access url with IncompleteRead, try number is...', e, _, url, mp.current_process() )
-            continue
-        except SocketError, e:
-            print('[WARN] Cannot access url with SocketError, try number is...', e, _, url, mp.current_process() )
-            continue
-        except UnicodeEncodeError, e:
-            print('[WARN] Cannot access url with UnicodeEncodeError, try number is...', e, _, url, mp.current_process() )
-            continue
-    #print "b"
-    if html == None:
-        return None
-    line = html.replace('\n', '^A^B^C')
-    line = regex.sub('<!--.*?-->', '',  line)
-    line = regex.sub('<style.*?/style>', '',  line)
-    html = regex.sub('<script.*?/script>', '', line ).replace('^A^B^C', ' ')
- 
-    #print "c"
-    soup = bs4.BeautifulSoup(html, "html.parser")
-    title = (lambda x:unicode(x.string) if x != None else 'Untitled')( soup.title )
-    # contents0_text = (lambda x:x.text.encode('utf-8') if x != None else "" ) \
-    #        ( soup.find('div', {'class': 'ui-section-body'}) )
-    contents0_text = (lambda x:x.text.encode('utf-8') if x != None else "" ) \
-            ( soup.find('article') )
-    #contents0_text = "dummy"
-    links = set([a['href'] for a in soup.find_all('a', href=True)])
-    return title, contents0_text, links
+def html_fetcher(url):
+  html = None
+  for _ in range(5):
+    opener = urllib.request.build_opener()
+    TIME_OUT = 10.
+    try:
+      html = opener.open(url, timeout = TIME_OUT).read()
+      break
+    except Exception as e:
+      print(e)
+  if html == None:
+    return None
+
+  soup = bs4.BeautifulSoup(html, "html.parser")
+  title = (lambda x:str(x.string) if x != None else 'Untitled')( soup.title )
+  contents0_text = (lambda x:x.text if x != None else "" ) \
+          ( soup.find('article') )
+  links = set([a['href'] for a in soup.find_all('a', href=True)])
+  return title, contents0_text, links
+
 html = open('./yomiuri.html').read()
 soup = bs4.BeautifulSoup(html, "html.parser")
-import plyvel
-import sys
-db = plyvel.DB('yomiuri.ldb', create_if_missing=True) 
-# クロウラーモード
+
+
+def parser(link):
+    if len(link) <= 2:
+      return None, link
+    
+    if link[0]  == '/':
+      link = "http://www.yomiuri.co.jp" + link
+    if "yomiuri" not in link:
+      print( link, "これは範囲外ドメインです" )
+      return None, link
+    try:
+      tp = html_fetcher(link)
+    except NotImplementedError as e:
+      return None, link
+    if tp == None:
+      return None, link
+    
+    title, contents, ls = tp
+    mtime = re.search(r"(\d\d\d\d年\d\d月\d\d日 \d\d時\d\d分)", contents)
+    if mtime is not None: 
+      with open("download/%s"%mtime.group(1), "w") as f:
+        f.write(contents)
+    print("正常に終了しました %s"%link)
+    return (ls, link)
+
+import concurrent.futures
+import pickle
+from multiprocessing import Process as Pr
 if '-c' in sys.argv:
-  from threading import Thread as T
-  from threading import active_count as CT
-  import time
-  import cPickle as P
-  links = P.loads(open('already_parsed_yomiuri.pkl').read())
-  [links.add(link) for link in set([a['href'] for a in soup.find_all('a', href=True)])]
+  try:
+    links, finished = pickle.loads( open("status.pkl", "rb").read() )
+    print("前回の状態から復旧します")
+  except:
+    finished = set()
+    links    = set()
+    [links.add(link) for link in set([a['href'] for a in soup.find_all('a', href=True)])]
 
   while links != set():
-    link = links.pop()
-    print "残リンク数", len(link)
-    if len(link) <= 2:
-        continue
-    if link[0]  == '/':
-        link = "http://www.yomiuri.co.jp" + link
-    if "yomiuri" not in link:
-        print link, "これは範囲外ドメインです"
-        continue
-    #print '[[' + str(link) + ']]'
-    if db.get(str(link)) != None: 
-       print link, "はすでにパース済みです"
-       continue
-    if db.get(str(link)) == None:
-      def runner():
-        tp = html_adhoc_fetcher(link)
-        if tp == None:
-          return
-        title, contents0, ls = tp
-        for l in ls:
-          utf8l = str(l)
-          if db.get(utf8l) == None and l not in links:
+    with concurrent.futures.ProcessPoolExecutor(max_workers=256) as executor:
+      for res in executor.map(parser, links):
+        ls, link = res
+        finished.add(link)
+        if ls is not None:
+          for l in ls:
+            if l not in finished:
               links.add(l)
-        print title, "パースしたよ, 残リンク数", len(links)
-        contents = contents0
-        db.put(str(link), contents )
-        #print str(link), contents
-      t = T(target=runner)
-      t.start()
-      print CT()
-      while CT() > 250 :
-        #time.sleep(0.001) 
-        pass
-      import cPickle as P
-      import random
-      if random.random() > 0.9:
-        open('already_parsed_yomiuri.pkl', 'w').write(P.dumps(links))
-# ダンプモード
-if '-d' in sys.argv:
-  for url, contents in db:
-    contents = contents.lower().replace('\n', '')
-    contents = regex.sub('　', ' ', contents)
-    contents = regex.sub('\s{1,}', ' ', contents)
-    contents = regex.sub('http.*?\s', '', contents)
-    for r in ['１', '２', '３', '４', '５', '６', '７', '８', '９', '０' ]:
-        contents = contents.replace(r, '')
-    contetns = regex.sub('\s{1,}', 'num', contents)
-    print contents
-
+    """ linksと、finishedの保存 """
+    print("メタ情報を保存しています")
+    open("status.pkl", "wb").write( pickle.dumps( (links, finished) ) )
+  """
+  for link in links: 
+    ls = parser( link ) 
+    finished.add( link ) 
+    if link not in finished:
+      links.add(link)
+  """
